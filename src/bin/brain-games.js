9f3b32ea663c6f0d8ded459ef1758f551b522805
muzzai <a.watalov@gmail.com>
2020-01-09 16:44:39 +0300

    trying to make import work

--- src/bin/brain-games.js
+++ src/bin/brain-games.js
@@ -1,2 +1,6 @@
-#!/usr/bin/node
+#!/snap/bin/node
+import { actual } from ".."
+
 console.log('Welcome to the Brain Games!');
+
+console.log(actual);
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import readlineSync from 'readline-sync'
+
+const actual = readlineSync.question('May I have your name, please: ');
+
+export default actual;
