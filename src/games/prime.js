deab79f08491bb48d7767bfced11e5ff4153113f
muzzai <a.watalov@gmail.com>
2020-02-06 00:30:35 +0300

    getting rid of cycle

--- src/games/prime.js
+++ src/games/prime.js
@@ -4,15 +4,12 @@ import playTheGame from '..';
 const gameDescription = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 
 const isPrime = (number) => {
-  if (number < 2) {
-    return false;
-  }
-  for (let counter = 2; counter <= Math.sqrt(number); counter += 1) {
-    if (number % counter === 0) {
-      return false;
-    }
-  }
-  return true;
+  const squareRootOfNumber = Math.sqrt(number);
+  const iter = (div) => {
+    if (div > squareRootOfNumber) return true;
+    return number % div === 0 ? false : iter(div + 1);
+  };
+  return iter(2);
 };
 
 const getGameData = () => getClosedQuestion(getRandomInt(0, 100), isPrime);
