6cca8f49e577f664f614f81b07977fb23fa3403d
muzzai <a.watalov@gmail.com>
2020-02-15 23:59:07 +0300

    refactoring

--- src/games/progression.js
+++ src/games/progression.js
@@ -7,18 +7,17 @@ const gameDescription = 'What number is missing in the progression?';
 const getGameData = () => {
   const step = getRandomInt(1, 100);
   const start = getRandomInt(0, 100);
-  const makeProgression = (prog, iterStep, len) => {
-    if (len === progressionLength) return prog;
+  const makeProgression = (prog, iterStep) => {
+    if (prog.length === progressionLength) return prog;
     const nextMember = prog[prog.length - 1] + iterStep;
-    return makeProgression([...prog, nextMember], iterStep, prog.length);
+    return makeProgression([...prog, nextMember], iterStep);
   };
   const progression = makeProgression([start], step);
   const hiddenMemberIndex = getRandomInt(0, progression.length - 1);
   const hiddenMember = String(progression[hiddenMemberIndex]);
   progression[hiddenMemberIndex] = '..';
-  const question = progression.join(' ');
   return {
-    question,
+    question: progression.join(' '),
     answer: hiddenMember,
   };
 };
